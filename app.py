280d6e3edadbff2246867a6deb2df0e270d1581a
faisalhumza132 <90932452+faisalhumza132@users.noreply.github.com>
2023-03-19 15:14:18 -0400

    Added template for main page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', )
 app.secret_key = 'mysecretkey'
 
 
